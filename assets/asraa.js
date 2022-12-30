05f227517100378146437f7eec6cf5f5f3995669
hayder <118937855+h94gg@users.noreply.github.com>
2022-12-30 22:50:42 +0300

    Add files via upload

---
+++ assets/asraa.js
@@ -0,0 +1,167 @@
+
+var images= ['assets/img/1.jpg','assets/img/2.jpg',
+'assets/img/3.jpg','assets/img/4.jpg',
+'assets/img/5.jpg','assets/img/7.jpg'];
+var i=0;
+function slideShow(){
+  document.getElementById('image').src=images[i];
+
+  if(i<images.length-1){
+    i++;
+
+  }
+  else{
+    i=0;
+
+  }
+  setTimeout("slideShow()",2000);
+}
+Window.onload=slideShow;
+
+
+// day-night1
+function night(){
+  document.getElementById('headercontainer').style.background='var(--nightheader)';
+  document.getElementById('day').style.display='none';
+  document.getElementById('body').style.background='var(--nightback)';
+  document.getElementById('body').style.color='var(--text-night)'; 
+  document.getElementById('night').style.display='none';
+  document.getElementById('day').style.display='block';
+  document.getElementById("hidmenu").style.background='var(--nightheader)';
+  document.getElementById("ul22").style.color='var(--text-night)';
+  document.getElementById("ul23").style.color='var(--text-night)';
+  document.getElementById("ul24").style.color='var(--text-night)';
+  document.getElementById("ul25").style.color='var(--text-night)';
+  document.getElementById("ul26").style.color='var(--text-night)';
+  document.getElementById("ul27").style.color='var(--text-night)';
+
+
+}
+function day(){
+  document.getElementById('headercontainer').style.background='var(--day-header)';
+  document.getElementById('body').style.background='var(--day-back)';
+  document.getElementById('body').style.color='var(--text-day)';  
+  document.getElementById('day').style.display='none';
+  document.getElementById('night').style.display='block';
+  document.getElementById("hidmenu").style.background='var(--day-header)';
+  document.getElementById("ul22").style.color='var(--text-day)';
+  document.getElementById("ul23").style.color='var(--text-day)';
+  document.getElementById("ul24").style.color='var(--text-day)';
+  document.getElementById("ul25").style.color='var(--text-day)';
+  document.getElementById("ul26").style.color='var(--text-day)';
+  document.getElementById("ul27").style.color='var(--text-day)';
+}
+
+
+
+// botton
+function show(){
+  document.getElementById('hidmenu').style.display='block';
+  document.getElementById('btnhid').style.display='block';
+  document.getElementById('btnshow').style.display='none';
+  
+
+    
+}
+function hide(){
+    document.getElementById('hidmenu').style.display='none';
+    document.getElementById('btnhid').style.display='none';
+    document.getElementById('btnshow').style.display='block';
+    
+      
+  }
+
+  var mq = window.matchMedia( "(max-width: 767px)" );
+  if (mq.matches) {
+      
+  function docbox1(){
+    document.getElementById('artdoc1').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc2').style.boxShadow= 'none';
+    document.getElementById('artdoc3').style.boxShadow= 'none';
+    document.getElementById('artdoc4').style.boxShadow= 'none';
+    document.getElementById('artdoc5').style.boxShadow= 'none';
+    document.getElementById('artdoc6').style.boxShadow= 'none';
+
+  }function docbox2(){
+    document.getElementById('artdoc2').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc1').style.boxShadow= 'none';
+    document.getElementById('artdoc3').style.boxShadow= 'none';
+    document.getElementById('artdoc4').style.boxShadow= 'none';
+    document.getElementById('artdoc5').style.boxShadow= 'none';
+    document.getElementById('artdoc6').style.boxShadow= 'none';
+
+  }function docbox3(){
+    document.getElementById('artdoc3').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc2').style.boxShadow= 'none';
+    document.getElementById('artdoc1').style.boxShadow= 'none';
+    document.getElementById('artdoc4').style.boxShadow= 'none';
+    document.getElementById('artdoc5').style.boxShadow= 'none';
+    document.getElementById('artdoc6').style.boxShadow= 'none';
+  }function docbox4(){
+    document.getElementById('artdoc4').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc2').style.boxShadow= 'none';
+    document.getElementById('artdoc3').style.boxShadow= 'none';
+    document.getElementById('artdoc1').style.boxShadow= 'none';
+    document.getElementById('artdoc5').style.boxShadow= 'none';
+    document.getElementById('artdoc6').style.boxShadow= 'none';
+
+  }function docbox5(){
+    document.getElementById('artdoc5').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc2').style.boxShadow= 'none';
+    document.getElementById('artdoc3').style.boxShadow= 'none';
+    document.getElementById('artdoc4').style.boxShadow= 'none';
+    document.getElementById('artdoc1').style.boxShadow= 'none';
+    document.getElementById('artdoc6').style.boxShadow= 'none';
+
+  }
+  function docbox6(){
+    document.getElementById('artdoc6').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('artdoc2').style.boxShadow= 'none';
+    document.getElementById('artdoc3').style.boxShadow= 'none';
+    document.getElementById('artdoc4').style.boxShadow= 'none';
+    document.getElementById('artdoc5').style.boxShadow= 'none';
+    document.getElementById('artdoc1').style.boxShadow= 'none';
+
+  }
+
+  function tr1(){
+    document.getElementById('tr1').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('tr2').style.boxShadow= 'none';
+    document.getElementById('tr3').style.boxShadow= 'none';
+    document.getElementById('tr4').style.boxShadow= 'none';
+    document.getElementById('tr5').style.boxShadow= 'none';
+  }
+  function tr2(){
+    document.getElementById('tr2').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('tr1').style.boxShadow= 'none';
+    document.getElementById('tr3').style.boxShadow= 'none';
+    document.getElementById('tr4').style.boxShadow= 'none';
+    document.getElementById('tr5').style.boxShadow= 'none';
+  } function tr3(){
+    document.getElementById('tr3').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('tr2').style.boxShadow= 'none';
+    document.getElementById('tr1').style.boxShadow= 'none';
+    document.getElementById('tr4').style.boxShadow= 'none';
+    document.getElementById('tr5').style.boxShadow= 'none';
+  } function tr4(){
+    document.getElementById('tr4').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('tr2').style.boxShadow= 'none';
+    document.getElementById('tr3').style.boxShadow= 'none';
+    document.getElementById('tr1').style.boxShadow= 'none';
+    document.getElementById('tr5').style.boxShadow= 'none';
+  } function tr5(){
+    document.getElementById('tr5').style.boxShadow= '0 0 7px 0 var(--color-box)';
+    document.getElementById('tr2').style.boxShadow= 'none';
+    document.getElementById('tr3').style.boxShadow= 'none';
+    document.getElementById('tr4').style.boxShadow= 'none';
+    document.getElementById('tr1').style.boxShadow= 'none';
+  }
+
+}
+  else {
+    // شجاي تسوي هنا امشي اطلع بره ؟؟؟
+}
+
+
+
+  
